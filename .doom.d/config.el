feeaf5e6c10fd162376b9b66190d118e805f3c3e
Velnbur <kirillbaybula@gmail.com>
2022-09-04 18:58:05 +0300

    make zshenv more extensive

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,60 +1,16 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Kyrylo Baibula"
       user-mail-address "kirillbaybula@gmail.com")
 
 
 (setq doom-font (font-spec :family "Iosevka" :size 15 :weight 'regular))
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type nil)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
-;; Enable tree-sitter
-;; (use-package! tree-sitter
-;;    :hook (prog-mode . turn-on-tree-sitter-mode)
-;;    :hook (tree-sitter-after-on . tree-sitter-hl-mode)
-;;    :config
-;;    (require 'tree-sitter-langs)
-;;    ;; This makes every node a link to a section of code
-;;    (setq tree-sitter-debug-jump-buttons t
-;;          ;; and this highlights the entire sub tree in your code
-;;          tree-sitter-debug-highlight-jump-region t))
 
 
 (setq doom-themes-treemacs-theme "doom-colors")
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,16 +1,25 @@
-. "$HOME/.cargo/env"
+if [[ -d "$HOME/.cargo" ]]; then
+	source "$HOME/.cargo/env"
+fi
 
-export PATH="$PATH:/opt/homebrew/bin"
+if [[ -d "/opt/homewbrew/bin" ]]; then
+	export PATH="$PATH:/opt/homebrew/bin"
+fi
 
+# Was needed before for emacs ~web-mode~
 alias tidy=/usr/local/bin/tidy
 
 if [[ -d "/usr/local/go" ]]; then
 	export GOROOT="/usr/local/go"
 	export PATH="$PATH:$GOROOT/bin"
-	export GOPATH="$HOME/Code/go" 
+	export GOPATH="$HOME/Code/go"
 	export PATH="$PATH:$GOPATH/bin"
 fi
 
 if [[ -d "$HOME/.emacs.d/bin" ]]; then
 	export PATH="$PATH:$HOME/.emacs.d/bin"
+fi
+
+if [[ -d "$HOME/.doom.d" ]]; then
+	export DOOMDIR="$PATH:$HOME/.doom.d"
 fi
